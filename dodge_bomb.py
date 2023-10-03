7eb2c485a1a1db8cc5eed361caa0a04bc38c4bca
C0A22079 <c0a220798d@edu.teu.ac.jp>
2023-10-03 14:43:05 +0900

    練習問題2

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -16,8 +16,9 @@ def main():
     bd_img.set_colorkey((0, 0, 0))
     pg.draw.circle(bd_img, (255, 0, 0), (10, 10), 10)
     bd_rct = bd_img.get_rect()
-    x, y = random.randint(0, 1600), random.randint(0, 900)
+    x, y = random.randint(0, WIDTH), random.randint(0, HEIGHT)
     bd_rct.center = (x, y)
+    vx, vy = 5, 5
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -27,10 +28,13 @@ def main():
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
-        screen.blit(bd_img, [x, y])
+        
+        bd_rct.move_ip(vx, vy)
+        screen.blit(bd_img, bd_rct)
+        
         pg.display.update()
         tmr += 1
-        clock.tick(10)
+        clock.tick(50)
 
 
 if __name__ == "__main__":
